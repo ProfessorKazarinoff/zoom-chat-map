172dea068c98d78d3b2d98b2234fa8338640edc4
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2020-10-14 11:59:05 -0700

    add zoom.py and zoom_credentials_example.json

---
+++ zoom.py
@@ -0,0 +1,14 @@
+import json
+from zoomus import ZoomClient
+
+with open(".zoom_credentials.json","r") as f:
+    json_secrets = json.load(f)
+    API_KEY = json_secrets['CLIENT_ID']
+    API_SECRET = json_secrets['CLIENT_SECRET']
+
+client = ZoomClient(API_KEY, API_SECRET)
+
+user_list_response = client.user.list()
+user_list = json.loads(user_list_response.content)
+
+print(user_list)
